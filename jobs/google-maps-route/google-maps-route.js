42ce5121468473a5a86e462f2900365a70fccb24
Matthias Spiller <himself@matthias-spiller.de>
2016-10-23 22:51:20 +0200

    Refactored completely

--- jobs/google-maps-route/google-maps-route.js
+++ jobs/google-maps-route/google-maps-route.js
@@ -1,23 +1,23 @@
 /**
- * Job: google-maps-route
- *
- * Expected configuration:
- *
- * ## PLEASE ADD AN EXAMPLE CONFIGURATION FOR YOUR JOB HERE
- * {
- *   myconfigKey : [
- *     { serverUrl : 'localhost' }
- *   ]
- * }
- */
+* Job: google-maps-route
+*
+* Expected configuration:
+*
+* ## PLEASE ADD AN EXAMPLE CONFIGURATION FOR YOUR JOB HERE
+* {
+*   myconfigKey : [
+*     { serverUrl : 'localhost' }
+*   ]
+* }
+*/
 
 module.exports = {
 
   /**
-   * Executed on job initialisation (only once)
-   * @param config
-   * @param dependencies
-   */
+  * Executed on job initialisation (only once)
+  * @param config
+  * @param dependencies
+  */
   onInit: function (config, dependencies) {
 
     /*
@@ -25,69 +25,82 @@ module.exports = {
 
     dependencies.logger.info('adding routes...');
     dependencies.app.route("/jobs/mycustomroute")
-        .get(function (req, res) {
-          res.end('So something useful here');
-        });
-    */
-  },
+    .get(function (req, res) {
+    res.end('So something useful here');
+  });
+  */
+},
 
-  /**
-   * Executed every interval
-   * @param config
-   * @param dependencies
-   * @param jobCallback
-   */
-  onRun: function (config, dependencies, jobCallback) {
+/**
+* Executed every interval
+* @param config
+* @param dependencies
+* @param jobCallback
+*/
+onRun: function (config, dependencies, jobCallback) {
 
-    /*
-     1. USE OF JOB DEPENDENCIES
+  /*
+  1. USE OF JOB DEPENDENCIES
 
-     You can use a few handy dependencies in your job:
+  You can use a few handy dependencies in your job:
 
-     - dependencies.easyRequest : a wrapper on top of the "request" module
-     - dependencies.request : the popular http request module itself
-     - dependencies.logger : atlasboard logger interface
+  - dependencies.easyRequest : a wrapper on top of the "request" module
+  - dependencies.request : the popular http request module itself
+  - dependencies.logger : atlasboard logger interface
 
-     Check them all out: https://bitbucket.org/atlassian/atlasboard/raw/master/lib/job-dependencies/?at=master
+  Check them all out: https://bitbucket.org/atlassian/atlasboard/raw/master/lib/job-dependencies/?at=master
 
-     */
+  */
 
-    var logger = dependencies.logger;
+  var logger = dependencies.logger;
 
-    /*
+  /*
 
-     2. CONFIGURATION CHECK
+  2. CONFIGURATION CHECK
 
-     You probably want to check that the right configuration has been passed to the job.
-     It is a good idea to cover this with unit tests as well (see test/google-maps-route file)
+  You probably want to check that the right configuration has been passed to the job.
+  It is a good idea to cover this with unit tests as well (see test/google-maps-route file)
 
-     Checking for the right configuration could be something like this:
+  Checking for the right configuration could be something like this:
 
-     if (!config.myrequiredConfig) {
-     return jobCallback('missing configuration properties!');
-     }
-
-
-     3. SENDING DATA BACK TO THE WIDGET
-
-     You can send data back to the widget anytime (ex: if you are hooked into a real-time data stream and
-     don't want to depend on the jobCallback triggered by the scheduler to push data to widgets)
-
-     jobWorker.pushUpdate({data: { title: config.widgetTitle, html: 'loading...' }}); // on Atlasboard > 1.0
+  if (!config.myrequiredConfig) {
+  return jobCallback('missing configuration properties!');
+}
 
 
-     4. USE OF JOB_CALLBACK
+3. SENDING DATA BACK TO THE WIDGET
 
-     Using nodejs callback standard conventions, you should return an error or null (if success)
-     as the first parameter, and the widget's data as the second parameter.
+You can send data back to the widget anytime (ex: if you are hooked into a real-time data stream and
+don't want to depend on the jobCallback triggered by the scheduler to push data to widgets)
 
-     This is an example of how to make an HTTP call to google using the easyRequest dependency,
-     and send the result to the registered widgets.
-     Have a look at test/google-maps-route for an example of how to unit tests this easily by mocking easyRequest calls
+jobWorker.pushUpdate({data: { title: config.widgetTitle, html: 'loading...' }}); // on Atlasboard > 1.0
 
-     */
 
-jobCallback(null, config);
+4. USE OF JOB_CALLBACK
 
-  }
+Using nodejs callback standard conventions, you should return an error or null (if success)
+as the first parameter, and the widget's data as the second parameter.
+
+This is an example of how to make an HTTP call to google using the easyRequest dependency,
+and send the result to the registered widgets.
+Have a look at test/google-maps-route for an example of how to unit tests this easily by mocking easyRequest calls
+
+*/
+
+var url = "https://maps.googleapis.com/maps/api/directions/json?";
+
+url += "origin=" + encodeURIComponent(config.source);
+url += "&destination=" + encodeURIComponent(config.destination);
+url += "&mode=driving&departure_time=now&alternatives=false";
+url += "&key=" + config.globalAuth['google-apis'].maps;
+
+var retVal = "";
+
+dependencies.easyRequest.HTML(url, function(err, json) {
+  // dependencies.logger.info(json);
+  var results = JSON.parse(json);
+
+  jobCallback(null, { "apikey": config.globalAuth['google-apis'].maps, "route" : results});
+});
+}
 };
--- widgets/google-maps-route/google-maps-route.js
+++ widgets/google-maps-route/google-maps-route.js
@@ -4,39 +4,49 @@ widget = {
   //runs when we receive data from the job
   onData: function (el, data) {
     if($('#google-maps-api-script').length == 0) {
-      $('head').append('<script id="google-maps-api-script" src="https://maps.googleapis.com/maps/api/js?key=' + data.globalAuth['google-apis'].maps + '" />');
+      $('head').append('<script id="google-maps-api-script" src="https://maps.googleapis.com/maps/api/js?libraries=geometry&key=' + data.apikey + '" />');
+
       return;
     }
 
-    var map = new google.maps.Map($(el).find(".map")[0], {
-      disableDefaultUI: true
+    config = data.config;
+
+    $('h2', el).text("ABC");
+
+    if (typeof google === 'object' && typeof google.maps === 'object') {
+      var map = new google.maps.Map($(el).find(".map")[0], {
+        disableDefaultUI: true
+      });
+      var trafficLayer = new google.maps.TrafficLayer();
+      trafficLayer.setMap(map);
+
+      directionsDisplay = new google.maps.DirectionsRenderer({
+        map: map
+      });
+
+    }
+    else {
+      return;
+    }
+
+    //directionsDisplay.setDirections(data.route);
+
+    var path = new google.maps.Polyline({
+      path: google.maps.geometry.encoding.decodePath(data.route.routes[0].overview_polyline.points),
+      geodesic: true,
+      strokeColor: '#0000DD',
+      strokeOpacity: 1.0,
+      strokeWeight: 4
     });
+    path.setMap(map);
 
+    var bounds = new google.maps.LatLngBounds();
+    var points = path.getPath().getArray();
+    for (var n = 0; n < points.length ; n++){
+      bounds.extend(points[n]);
+    }
+    map.fitBounds(bounds);
 
-    var trafficLayer = new google.maps.TrafficLayer();
-    trafficLayer.setMap(map);
-
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-      map: map
-    });
-
-    // Set destination, origin and travel mode.
-    var request = {
-      destination: data.destination,
-      origin: data.source,
-      travelMode: 'DRIVING',
-      drivingOptions: {
-        departureTime: new Date(Date.now())
-      }
-    };
-
-    // Pass the directions request to the directions service.
-    var directionsService = new google.maps.DirectionsService();
-    directionsService.route(request, function(response, status) {
-      if (status == 'OK') {
-        // Display the route on the map.
-        directionsDisplay.setDirections(response);
-      }
-    });
+    $('h2', el).text(data.config.title + ": " + data.route.routes[0].legs[0].duration_in_traffic.text);
   }
 };
